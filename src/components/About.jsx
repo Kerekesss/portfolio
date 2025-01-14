2eb570c43edb2f35bd398402ff7c139117dfa6ae
Kerekesss <151868135+Kerekesss@users.noreply.github.com>
2025-01-14 11:30:17 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,10 +19,10 @@ const About = () => {
           </div>
           <div>
             <p>
-            My journey into web development began with FreeCodeCamp, where I created my first front-end projects and laid a strong foundation in HTML and CSS. <br> 
-            As I progressed, I expanded my skills by diving into JavaScript, eventually embracing React.js to build dynamic, interactive web applications. <br>
-            To strengthen my back-end expertise, I completed an in-depth PHP course under the guidance of a senior developer. This training allowed me to create my first full-stack website, combining PHP and MySQL to manage data and functionality effectively. <br>
-            Over time, I added tools like Tailwind CSS to my skillset, enhancing my ability to design clean, responsive user interfaces. <br>
+            My journey into web development began with FreeCodeCamp, where I created my first front-end projects and laid a strong foundation in HTML and CSS. <br /> 
+            As I progressed, I expanded my skills by diving into JavaScript, eventually embracing React.js to build dynamic, interactive web applications. <br />
+            To strengthen my back-end expertise, I completed an in-depth PHP course under the guidance of a senior developer. This training allowed me to create my first full-stack website, combining PHP and MySQL to manage data and functionality effectively. <br />
+            Over time, I added tools like Tailwind CSS to my skillset, enhancing my ability to design clean, responsive user interfaces. <br />
             I like programming because it helps solve problems and create useful solutions. Every project teaches me something new and helps me improve my skills.
             </p>
           </div>
